50ce8d1d4bd2dd9529c8432ec67b700df27fb4bb
jordanmccomb712 <jordanmccomb712@gmail.com>
2023-04-05 16:12:55 +0100

    rollback

--- Models/Fine.cs
+++ Models/Fine.cs
@@ -3,11 +3,11 @@ public class Fine{
     public string FineID {get; set; }
     public string FullName {get; set;}
     public double AmountDue {get; set;} //in GBP
-    public DateOnly DateIssued {get; set;}
+    public DateTime DateIssued {get; set;}
 
     public bool IsPaid {get; set;}
 
-    public Fine(string fineID, string fullName, double amountDue, DateOnly dateIssued, bool isPaid)
+    public Fine(string fineID, string fullName, double amountDue, DateTime dateIssued, bool isPaid)
     {
         this.FineID = fineID;
         this.FullName = fullName;
